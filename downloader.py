5ebdba0d45bf860f5060b74c261dc1d1431c6e06
hamdyaea <hamdy.aea@protonmail.com>
2022-01-12 09:03:25 +0100

    reload api

--- downloader.py
+++ downloader.py
@@ -5,53 +5,71 @@
 import requests
 import json
 import datetime
+import sys
 
 
 current_date = datetime.date.today()
 current_date = current_date.strftime("%d-%m-%Y")
 
+
+def reload():
+    username = "hamdyaea"
+    token = "46e780ca68e2c38ff7548a9e76e907b51013d02d"
+
+    response = requests.get(
+        "https://www.pythonanywhere.com/api/v0/user/{username}/reload/".format(
+            username=username
+        ),
+        headers={"Authorization": "Token {token}".format(token=token)},
+    )
+
+
 def main():
-    
+
     url = "https://unogs-unogs-v1.p.rapidapi.com/aaapi.cgi"
 
-    querystring = {"q":"get:new7:CH","p":"1","t":"ns","st":"adv"}
+    querystring = {"q": "get:new7:CH", "p": "1", "t": "ns", "st": "adv"}
 
     headers = {
-    'x-rapidapi-host': "unogs-unogs-v1.p.rapidapi.com",
-    'x-rapidapi-key': "YOUR-KEY-HERE"
+        "x-rapidapi-host": "unogs-unogs-v1.p.rapidapi.com",
+        "x-rapidapi-key": "a1296b58a5msh0d1514001b88f9ep1afbcajsncfb2e503e33b",
     }
 
     data = requests.request("GET", url, headers=headers, params=querystring)
     data = data.text
-    
-    #ListNewRaw = data.decode("utf-8")
+
+    # ListNewRaw = data.decode("utf-8")
     ListNewJson = json.loads(data)
     total = ListNewJson["ITEMS"]
-    totalcount =  ListNewJson["COUNT"]
+    totalcount = ListNewJson["COUNT"]
     movielist = []
     for i in total:
-        topaste = str("    Date : ")+str(i["unogsdate"])+str("     Title : ")+str(i["title"])
+        topaste = (
+            str("    Date : ")
+            + str(i["unogsdate"])
+            + str("     Title : ")
+            + str(i["title"])
+        )
         movielist.append(topaste)
-    
-    datef = open("/home/hamdyaea/mysite/update.txt",'w')
+
+    datef = open("/home/hamdyaea/mysite/update.txt", "w")
     datef.write(str(current_date))
     datef.close()
-    
-    countf = open("/home/hamdyaea/mysite/counter.txt",'w')
+
+    countf = open("/home/hamdyaea/mysite/counter.txt", "w")
     countf.write(str(totalcount))
     countf.close()
-    
-    movief = open("/home/hamdyaea/mysite/movies.txt","w")
+
+    movief = open("/home/hamdyaea/mysite/movies.txt", "w")
     movief.write("")
     movief.close()
-    
-   
+
     for i in movielist:
-        movief = open("/home/hamdyaea/mysite/movies.txt","a")
+        movief = open("/home/hamdyaea/mysite/movies.txt", "a")
         movief.write(str(i))
         movief.write("\n")
-        movief.close()        
-        
-    
+        movief.close()
+
 
 main()
+reload()
